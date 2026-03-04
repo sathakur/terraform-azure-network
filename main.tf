7cc9ffd0df689ac0707fcad197df5ef08743aab1
Thakur <sanjeev.thakur@capgemini.com>
2026-03-04 14:44:58 +0530

    Initial Terraform with GitHub Actions

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# -------------------------
+# Resource Group
+# -------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-network-weu"
+  location = "westeurope"
+}
+
+# -------------------------
+# Virtual Network
+# -------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-weu-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# -------------------------
+# Subnet
+# -------------------------
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-app-01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
