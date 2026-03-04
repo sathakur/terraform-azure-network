b90b4be268c7210eca2ef3b4a4cae207fa4e33a1
Sanjeev Thakur <sanjeev.thakur@capgemini.com>
2026-03-04 19:44:14 +0530

    remove subnet

--- main.tf
+++ main.tf
@@ -39,13 +39,3 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-# -------------------------
-# Subnet
-# -------------------------
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-app-01"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-}
