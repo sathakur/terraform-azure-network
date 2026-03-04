83ae5c517feb055e94dbd237ab0af9679b66d89b
Sanjeev Thakur <sanjeev.thakur@capgemini.com>
2026-03-04 15:40:52 +0530

    add subnet02

--- main.tf
+++ main.tf
@@ -39,4 +39,11 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+resource "azurerm_subnet" "subnet02" {
+  name                 = "subnet-app-02"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
 }
