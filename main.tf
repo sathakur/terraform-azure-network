6f6ba644913ce758fd2b18800b1668c37f4bc7c2
Sanjeev Thakur <sanjeev.thakur@capgemini.com>
2026-03-04 15:41:46 +0530

    correct syntax

--- main.tf
+++ main.tf
@@ -40,6 +40,8 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
+}
+
 resource "azurerm_subnet" "subnet02" {
   name                 = "subnet-app-02"
   resource_group_name  = azurerm_resource_group.rg.name
