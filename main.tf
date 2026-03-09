cfcaa8a2079342c57aeabc01e0d278217c1b392a
Sanjeev Thakur <sanjeev.thakur@capgemini.com>
2026-03-09 17:03:57 +0530

    with VM config

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ terraform {
     key                  = "network.terraform.tfstate"
   }
 
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -39,3 +38,96 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
+# -------------------------
+# Subnet
+# -------------------------
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# -------------------------
+# Network Security Group
+# -------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-ubuntu-weu"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.0.0.0/16"
+    destination_address_prefix = "*"
+  }
+}
+
+# -------------------------
+# Associate NSG with Subnet
+# -------------------------
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -------------------------
+# Network Interface
+# -------------------------
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "nic-ubuntu-weu"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# -------------------------
+# Ubuntu Spot Virtual Machine
+# -------------------------
+resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
+  name                = "vm-ubuntu-weu"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  size     = "Standard_B1s"
+  priority = "Spot"
+
+  eviction_policy = "Deallocate"
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id
+  ]
+
+  os_disk {
+    name                 = "ubuntu-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  disable_password_authentication = true
+}
